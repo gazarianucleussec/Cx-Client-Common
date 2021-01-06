9f40b2a6212418f3075a54bbfb863833509800a3
orlyk <orly.kilidgan@checkmarx.com>
2021-01-06 14:43:14 +0200

    fix sonar issue

--- src/main/java/com/cx/restclient/ast/AstScaClient.java
+++ src/main/java/com/cx/restclient/ast/AstScaClient.java
@@ -215,14 +215,19 @@ public class AstScaClient extends AstClient implements Scanner {
     @Override
     protected void uploadArchive(byte[] source, String uploadUrl) throws IOException {
         log.info("Uploading the zipped data.");
-
+        CxHttpClient uploader = null;
         HttpEntity request = new ByteArrayEntity(source);
 
-        CxHttpClient uploader = createHttpClient(uploadUrl);
+        try {
+            uploader = createHttpClient(uploadUrl);
 
-        // Relative path is empty, because we use the whole upload URL as the base URL for the HTTP client.
-        // Content type is empty, because the server at uploadUrl throws an error if Content-Type is non-empty.
-        uploader.putRequest("", "", request, JsonNode.class, HttpStatus.SC_OK, "upload ZIP file");
+            // Relative path is empty, because we use the whole upload URL as the base URL for the HTTP client.
+            // Content type is empty, because the server at uploadUrl throws an error if Content-Type is non-empty.
+            uploader.putRequest("", "", request, JsonNode.class, HttpStatus.SC_OK, "upload ZIP file");
+        }finally {
+            Optional.ofNullable(uploader).ifPresent(CxHttpClient::close);
+        }
+
     }
     
     @Override
